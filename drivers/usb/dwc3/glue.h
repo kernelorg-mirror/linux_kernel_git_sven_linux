393169992e554a82df358f8046670fe68d55020f
Sven Peter <sven@kernel.org>
2025-09-05 19:24:12 +0000

    usb: dwc3: glue: Allow more fine grained control over mode switches
    We need fine grained control over mode switched on the DWC3 controller
    present on Apple Silicon. Export core, host and gadget init and exit,
    ptrcap and susphy control functions. Also introduce an additional
    parameter to probe_data that allows to skip the final initialization
    step that would bring up host or gadget mode.
    
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/usb/dwc3/glue.h
+++ drivers/usb/dwc3/glue.h
@@ -15,15 +15,29 @@
  * @res: resource for the DWC3 core mmio region
  * @ignore_clocks_and_resets: clocks and resets defined for the device should
  *		be ignored by the DWC3 core, as they are managed by the glue
+ * @skip_core_init_mode: skip the finial initialization of the target mode, as
+ *		it must be managed by the glue
  */
 struct dwc3_probe_data {
 	struct dwc3 *dwc;
 	struct resource *res;
 	bool ignore_clocks_and_resets;
+	bool skip_core_init_mode;
 };
 
 int dwc3_core_probe(const struct dwc3_probe_data *data);
 void dwc3_core_remove(struct dwc3 *dwc);
+
+int dwc3_core_init(struct dwc3 *dwc);
+void dwc3_core_exit(struct dwc3 *dwc);
+
+int dwc3_host_init(struct dwc3 *dwc);
+void dwc3_host_exit(struct dwc3 *dwc);
+int dwc3_gadget_init(struct dwc3 *dwc);
+void dwc3_gadget_exit(struct dwc3 *dwc);
+
+void dwc3_enable_susphy(struct dwc3 *dwc, bool enable);
+void dwc3_set_prtcap(struct dwc3 *dwc, u32 mode, bool ignore_susphy);
 
 int dwc3_runtime_suspend(struct dwc3 *dwc);
 int dwc3_runtime_resume(struct dwc3 *dwc);
