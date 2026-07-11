3eade8088fcd4488067520c4262cfaffdfe4eb93
Sven Peter <sven@kernel.org>
2026-07-11 23:50:37 +0200

    firmware/psci: Add EFI runtime conduit
    Apple Silicon machines run the kernel at the highest available exception
    level, leaving no higher level to trap into for PSCI firmware calls. The
    firmware implementing PSCI therefore shares that level with the kernel and
    is exposed as an EFI runtime service instead of being reached through an
    SMC or HVC trap.
    
    Add a new "efi" conduit that routes PSCI calls to the handler extracted
    from the custom EFI configuration table. The method is rejected if the
    kernel has no EFI support or if the firmware did not provide the handler
    table.
    
    PSCI_VERSION, MIGRATE_INFO_TYPE and PSCI_FEATURES are queried during early
    CPU bring-up before EFI runtime services are available and have to be
    answered without calling into the firmware: PSCI_VERSION and PSCI_FEATURES
    are answered from the firmware-provided table while MIGRATE_INFO_TYPE is
    hardcoded to PSCI_0_2_TOS_MP since no Trusted OS is present on these
    machines.
    
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/firmware/psci/psci.c
+++ drivers/firmware/psci/psci.c
@@ -10,6 +10,7 @@
 #include <linux/arm-smccc.h>
 #include <linux/cpuidle.h>
 #include <linux/debugfs.h>
+#include <linux/efi.h>
 #include <linux/errno.h>
 #include <linux/linkage.h>
 #include <linux/of.h>
@@ -24,6 +25,7 @@
 
 #include <asm/cpuidle.h>
 #include <asm/cputype.h>
+#include <asm/efi.h>
 #include <asm/hypervisor.h>
 #include <asm/system_misc.h>
 #include <asm/smp_plat.h>
@@ -54,6 +56,7 @@ enum psci_conduit {
 	PSCI_CONDUIT_NONE,
 	PSCI_CONDUIT_SMC,
 	PSCI_CONDUIT_HVC,
+	PSCI_CONDUIT_EFI,
 };
 
 static enum psci_conduit psci_conduit = PSCI_CONDUIT_NONE;
@@ -137,6 +140,72 @@ __invoke_psci_fn_smc(unsigned long function_id,
 	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
 	return res.a0;
 }
+
+#if IS_ENABLED(CONFIG_EFI) && IS_ENABLED(CONFIG_ARM64)
+static bool efi_psci_available(void)
+{
+	return efi_psci.psci_handler;
+}
+
+static bool efi_psci_fn_valid(unsigned long function_id)
+{
+	if (function_id >= PSCI_0_2_FN_BASE &&
+	    function_id < PSCI_0_2_FN(EFI_PSCI_MAX_FN))
+		return true;
+
+	if (function_id >= PSCI_0_2_FN64_BASE &&
+	    function_id < PSCI_0_2_FN64(EFI_PSCI_MAX_FN))
+		return true;
+
+	return false;
+}
+
+static unsigned long __invoke_psci_fn_efi(unsigned long function_id,
+					  unsigned long arg0,
+					  unsigned long arg1,
+					  unsigned long arg2)
+{
+	u32 fn;
+
+	/* These are called before EFI runtime services are available */
+	switch (function_id) {
+	case PSCI_0_2_FN_PSCI_VERSION:
+		return efi_psci.version;
+	case PSCI_0_2_FN_MIGRATE_INFO_TYPE:
+		/* No Trusted OS is present, migration is never required */
+		return PSCI_0_2_TOS_MP;
+	case PSCI_1_0_FN_PSCI_FEATURES:
+		if (!efi_psci_fn_valid(arg0))
+			return PSCI_RET_NOT_SUPPORTED;
+		fn = arg0 & 0xff;
+		return efi_psci.features[fn];
+	}
+
+	if (!efi_psci_fn_valid(function_id))
+		return PSCI_RET_NOT_SUPPORTED;
+
+	if (WARN_ON_ONCE(!efi_psci.psci_handler))
+		return PSCI_RET_NOT_SUPPORTED;
+	if (WARN_ON_ONCE(!efi_enabled(EFI_RUNTIME_SERVICES)))
+		return PSCI_RET_NOT_SUPPORTED;
+
+	return arm64_efi_psci_call(function_id, arg0, arg1, arg2);
+}
+#else
+static bool efi_psci_available(void)
+{
+	return false;
+}
+
+static unsigned long __invoke_psci_fn_efi(unsigned long function_id,
+					  unsigned long arg0,
+					  unsigned long arg1,
+					  unsigned long arg2)
+{
+	WARN(1, "EFI PSCI conduit invoked but kernel has no EFI support");
+	return PSCI_RET_NOT_SUPPORTED;
+}
+#endif
 
 static __always_inline int psci_to_linux_errno(int errno)
 {
@@ -284,6 +353,9 @@ static void set_conduit(enum psci_conduit conduit)
 	case PSCI_CONDUIT_SMC:
 		invoke_psci_fn = __invoke_psci_fn_smc;
 		break;
+	case PSCI_CONDUIT_EFI:
+		invoke_psci_fn = __invoke_psci_fn_efi;
+		break;
 	default:
 		WARN(1, "Unexpected PSCI conduit %d\n", conduit);
 	}
@@ -306,6 +378,12 @@ static int get_set_conduit_method(const struct device_node *np)
 		set_conduit(PSCI_CONDUIT_HVC);
 	} else if (!strcmp("smc", method)) {
 		set_conduit(PSCI_CONDUIT_SMC);
+	} else if (!strcmp("efi", method)) {
+		if (!efi_psci_available()) {
+			pr_warn("EFI conduit is not available\n");
+			return -EINVAL;
+		}
+		set_conduit(PSCI_CONDUIT_EFI);
 	} else {
 		pr_warn("invalid \"method\" property: %s\n", method);
 		return -EINVAL;
@@ -660,6 +738,9 @@ static void __init psci_init_smccc(void)
 	case PSCI_CONDUIT_SMC:
 		conduit = SMCCC_CONDUIT_SMC;
 		break;
+	case PSCI_CONDUIT_EFI:
+		/* EFI calls cannot be expressed as an SMCCC conduit */
+		return;
 	default:
 		conduit = SMCCC_CONDUIT_NONE;
 		break;
